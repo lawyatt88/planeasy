5f2583e10476a60907ea376b742567b8e00bbcf6
LeMona Wyatt <lemonawyatt@gmail.com>
2018-11-11 18:25:41 -0500

    initial commit

--- client/app.js
+++ client/app.js
@@ -1,15 +1,49 @@
 import React from 'react'
 
-import {Navbar} from './components'
+import {MainNav} from './components'
 import Routes from './routes'
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
+class App extends React.Component {
+  state = {navSize: {}}
+
+  componentDidMount() {
+    this.doReportSize = true
+  }
+
+  componentDidUpdate() {
+    if (this.doReportSize) {
+      this.setNavSize(this.elementRef.getBoundingClientRect())
+      console.log({
+        ref: this.elementRef,
+        size: this.elementRef.getBoundingClientRect()
+      })
+      this.doReportSize = false
+    }
+  }
+
+  setNavSize = navSize => this.setState({navSize})
+
+  refCallback = element => {
+    if (element) {
+      this.elementRef = element
+      this.setNavSize(element.getBoundingClientRect())
+    }
+  }
+
+  render() {
+    const {navSize} = this.state
+    return (
+      <div>
+        <MainNav refCallback={this.refCallback} />
+        <div
+          className="container-fluid"
+          style={{paddingTop: this.state.navSize.height}}
+        >
+          <Routes navHeight={this.state.navSize.height} />
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App
--- client/components/index.js
+++ client/components/index.js
@@ -3,6 +3,8 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Navbar} from './navbar'
+export {default as MainNav} from './navbar'
 export {default as UserHome} from './user-home'
+export {default as HowItWorks} from './HowItWorks'
+export {default as Home} from './Home'
 export {Login, Signup} from './auth-form'
